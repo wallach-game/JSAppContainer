828f887a87a4ab0992dfac9b6ce286cece2ad0e2
wallach-game <jirklym@gmail.com>
2021-03-14 15:25:40 +0100

    live server added
    Project structure need to change, or advanced install scripts needed

--- App/JS/Core/Core.js
+++ App/JS/Core/Core.js
@@ -1,1 +1,9 @@
+import * as Window from './CoreObjects/Window.js'
+
 console.log('Core Loaded');
+
+
+const windowObjectTest = new Window();
+console.log(windowObjectTest);
+
+//TODO: ReworkProject structure.
