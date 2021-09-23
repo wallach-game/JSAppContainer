2c4971b465a43b3bb3aba7b14d0d7d1e75bf0f7a
Jiří Kořenek <jirklym@gmail.com>
2021-09-23 16:17:07 +0200

    instgram window cahnge

--- bin/App/JS/Core/Core.js
+++ bin/App/JS/Core/Core.js
@@ -4,14 +4,15 @@ import CWindow from './CoreObjects/CWindow.js';
 console.log('Core Loaded');
 
 CWindow.MainWindow = window;
-//CWindow.MainWindow.close();
 
-var win = new CWindow("test","",true);
+
+var win = new CWindow("test","https://instagram.com",true);
 //win.CloseWindow();
 
 
 
 //comment this out when u dont wanna new windows after saving
+//CWindow.MainWindow.close();
 //window.close();
 
 
