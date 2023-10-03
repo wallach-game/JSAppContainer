97537ef3572b89cbd9e38027025748c35b06cf45
Jiri Korenek <jirklym@gmail.com>
2023-10-03 14:08:21 +0200

    icons added

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
-const buttons = document.querySelectorAll('mdc-button'); 
-for (const button of buttons) 
-{ 
-    mdc.ripple.MDCRipple.attachTo(button); 
-}
-// This will add a ripple to all of the buttons.
-
-// const buttons = document.querySelectorAll('.mdc-button');
-// for (const button of buttons) {
-//   mdc.ripple.MDCRipple.attachTo(button);
+// const buttons = document.querySelectorAll('mdc-button'); 
+// for (const button of buttons) 
+// { 
+//     mdc.ripple.MDCRipple.attachTo(button); 
 // }
+// // This will add a ripple to all of the buttons.
+
+// // const buttons = document.querySelectorAll('.mdc-button');
+// // for (const button of buttons) {
+// //   mdc.ripple.MDCRipple.attachTo(button);
+// // }
